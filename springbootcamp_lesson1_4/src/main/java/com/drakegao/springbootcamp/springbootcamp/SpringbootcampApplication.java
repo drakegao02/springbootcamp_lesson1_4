65d5af548ce0f394d5b517a4bfc2f9f4774a8842
drakegao02 <drakegaopro@gmail.com>
2017-08-22 10:17:16 -0400

    lesson 1 to 3

---
+++ springbootcamp_lesson1_4/src/main/java/com/drakegao/springbootcamp/springbootcamp/SpringbootcampApplication.java
@@ -0,0 +1,12 @@
+package com.drakegao.springbootcamp.springbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcampApplication.class, args);
+	}
+}
